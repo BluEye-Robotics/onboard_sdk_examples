06457afc1a8d286c8a69ca0d44d947d6bbcccbf0
johannesschrimpf <johannesschrimpf@users.noreply.github.com>
2024-01-31 12:32:41 +0000

    deploy: BluEye-Robotics/libblunux@7c59566c311a67181fcb518db773456fe4340fc6

--- search/all_8.js
+++ search/all_8.js
@@ -6,8 +6,10 @@ var searchData=
   ['indicatorssetpoint_3',['IndicatorsSetpoint',['../structblunux_1_1pmu_1_1_indicators_setpoint.html',1,'blunux::pmu']]],
   ['iobserverstate_4',['IObserverState',['../classblunux_1_1observer_1_1_i_observer_state.html',1,'blunux::observer']]],
   ['ipmucallbacks_5',['IPMUCallbacks',['../classblunux_1_1pmu_1_1_i_p_m_u_callbacks.html',1,'blunux::pmu']]],
-  ['iscalibrating_6',['isCalibrating',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a4ef5ee091c49e891628586fd5e934da7',1,'blunux::observer::ICompassCalibration']]],
-  ['iscalibrationfinished_7',['isCalibrationFinished',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a722f5e9abc272546927afd340c3ef2f2',1,'blunux::observer::ICompassCalibration']]],
-  ['isreadyforthrustercalibration_8',['isReadyForThrusterCalibration',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a575a7391ec9c17538702f2ba559f4584',1,'blunux::observer::ICompassCalibration']]],
-  ['itiltcontrol_9',['ITiltControl',['../classblunux_1_1tilt_1_1_i_tilt_control.html',1,'blunux::tilt']]]
+  ['is_5fcalibrated_6',['is_calibrated',['../classblunux_1_1tilt_1_1_i_tilt_control.html#aff0a1c47b6a9ada8cfb48dc0833a59f6',1,'blunux::tilt::ITiltControl']]],
+  ['is_5fstabalized_7',['is_stabalized',['../classblunux_1_1tilt_1_1_i_tilt_control.html#a205135640f861c9a6b9dc16b2890e169',1,'blunux::tilt::ITiltControl']]],
+  ['iscalibrating_8',['isCalibrating',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a4ef5ee091c49e891628586fd5e934da7',1,'blunux::observer::ICompassCalibration']]],
+  ['iscalibrationfinished_9',['isCalibrationFinished',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a722f5e9abc272546927afd340c3ef2f2',1,'blunux::observer::ICompassCalibration']]],
+  ['isreadyforthrustercalibration_10',['isReadyForThrusterCalibration',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a575a7391ec9c17538702f2ba559f4584',1,'blunux::observer::ICompassCalibration']]],
+  ['itiltcontrol_11',['ITiltControl',['../classblunux_1_1tilt_1_1_i_tilt_control.html',1,'blunux::tilt']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['iscalibrating_0',['isCalibrating',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a4ef5ee091c49e891628586fd5e934da7',1,'blunux::observer::ICompassCalibration']]],
-  ['iscalibrationfinished_1',['isCalibrationFinished',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a722f5e9abc272546927afd340c3ef2f2',1,'blunux::observer::ICompassCalibration']]],
-  ['isreadyforthrustercalibration_2',['isReadyForThrusterCalibration',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a575a7391ec9c17538702f2ba559f4584',1,'blunux::observer::ICompassCalibration']]]
+  ['is_5fcalibrated_0',['is_calibrated',['../classblunux_1_1tilt_1_1_i_tilt_control.html#aff0a1c47b6a9ada8cfb48dc0833a59f6',1,'blunux::tilt::ITiltControl']]],
+  ['is_5fstabalized_1',['is_stabalized',['../classblunux_1_1tilt_1_1_i_tilt_control.html#a205135640f861c9a6b9dc16b2890e169',1,'blunux::tilt::ITiltControl']]],
+  ['iscalibrating_2',['isCalibrating',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a4ef5ee091c49e891628586fd5e934da7',1,'blunux::observer::ICompassCalibration']]],
+  ['iscalibrationfinished_3',['isCalibrationFinished',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a722f5e9abc272546927afd340c3ef2f2',1,'blunux::observer::ICompassCalibration']]],
+  ['isreadyforthrustercalibration_4',['isReadyForThrusterCalibration',['../classblunux_1_1observer_1_1_i_compass_calibration.html#a575a7391ec9c17538702f2ba559f4584',1,'blunux::observer::ICompassCalibration']]]
 ];
